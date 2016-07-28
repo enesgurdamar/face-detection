f49c3e3d9d226212995ba507abe5839af0619e4d
Enes Nehir Gürdamar <nehir.gurdamar@bilgiedu.net>
2016-07-28 17:30:18 +0300

    View image & Capture web cam

--- src/main/java/com/enderunlabs/app/App.java
+++ src/main/java/com/enderunlabs/app/App.java
@@ -1,23 +1,96 @@
 package com.enderunlabs.app;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.opencv.core.Scalar;
+import java.awt.Image;
+
+import javax.swing.*;
+
+import com.enderunlabs.app.utils.ImageProcessor;
+import com.enderunlabs.app.utils.ImageViewer;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.Videoio;
+import org.opencv.videoio.VideoCapture;
 
 public class App {
 
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
+    private JFrame frame;
+    private JLabel imageLabel;
+
     public static void main( String[] args ) {
 
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+        // Open an image file
+        String filePath = "src/main/resources/images/starrynight.jpg";
+        Mat newImage = Imgcodecs.imread(filePath);
+        if(newImage.dataAddr()==0){
+            System.out.println("Couldn't open file " + filePath);
+        }
+        else {
+            ImageViewer imageViewer = new ImageViewer();
+            imageViewer.show(newImage, "Loaded Image");
+        }
+
+        App app = new App();
+        app.initGUI();
+        app.runMainLoop(args);
+
     }
+
+    // Set the blue channel to zero (BGR)
+    public void filter(Mat image){
+
+        int totalBytes = (int)(image.total() * image.elemSize()); // Number of bytes in the image
+        byte[] buffer = new byte[totalBytes];
+        image.get(0,0,buffer); // Copy the matrix contents in byte array buffer
+        for (int i=0; i<totalBytes; i++){
+            if (i%3==0) // Refers to the blue channel
+                buffer[i] = 0;
+        }
+        image.put(0,0,buffer); // Copy the byte array to native storage
+    }
+
+    private void initGUI(){
+
+        frame = new JFrame("Camera Input Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        imageLabel = new JLabel();
+        frame.add(imageLabel);
+        frame.setVisible(true);
+    }
+
+    // Capture a video from a camera
+    private void runMainLoop(String[] args){
+
+        ImageProcessor imageViewer = new ImageProcessor();
+        Mat webcamMatImage = new Mat();
+        Image tempImage;
+        VideoCapture capture = new VideoCapture(0); // Capture from web cam (0)
+        // VideoCapture capture = new VideoCapture("rtsp://192.168.2.64/Streaming/Channels/101");
+        // Setting camera resolution
+        capture.set(Videoio.CAP_PROP_FRAME_WIDTH,800);
+        capture.set(Videoio.CAP_PROP_FRAME_HEIGHT,600);
+        if (capture.isOpened()){ // Check whether the camera is instantiated
+            while (true){ // Retrieve each captured frame in a loop
+                capture.read(webcamMatImage);
+                if (!webcamMatImage.empty()){
+                    tempImage = imageViewer.toBufferedImage(webcamMatImage);
+                    ImageIcon imageIcon = new ImageIcon(tempImage, "Captured Video");
+                    imageLabel.setIcon(imageIcon);
+                    frame.pack(); // This will resize the window to fit the image
+                }
+                else {
+                    System.out.println(" -- Frame not captured -- Break!");
+                    break;
+                }
+            }
+        }
+        else {
+            System.out.println("Couldn't open capture.");
+        }
+    }
+
+
+
 }
