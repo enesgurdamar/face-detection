050c66ab452cb43f6d5ee5ae3944ad54cd68b733
engkrbdk <e.k.1905@hotmail.com>
2016-08-10 11:45:02 +0300

    eye detection

--- src/main/java/com/enderunlabs/app/App.java
+++ src/main/java/com/enderunlabs/app/App.java
@@ -1,23 +1,20 @@
 package com.enderunlabs.app;
 
-import java.awt.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-
 import com.enderunlabs.app.utils.ImageProcessor;
 import org.opencv.core.*;
 import org.opencv.core.Point;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.videoio.Videoio;
 import org.opencv.videoio.VideoCapture;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.Videoio;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.opencv.objdetect.Objdetect.CASCADE_SCALE_IMAGE;
 
 public class App extends Thread {
 
@@ -30,9 +27,13 @@ public class App extends Thread {
     private CascadeClassifier faceDetector;
     private JButton btnRecognize;
     private JPanel buttonPanel;
-
+private CascadeClassifier eyeDetector;
     public static void main(String[] args) throws InterruptedException, IOException {
 
+//        File f = new File("data/engin.png");
+//        File f2 = new File("data.png");
+//        Compare imgComp = new Compare();
+//        System.out.println(imgComp.compareTwoImages(f, f2));
         // Capture video
         App app = new App();
         app.initGUI();
@@ -44,6 +45,8 @@ public class App extends Thread {
     private void loadCascade() {
         String cascadePath = "src/main/resources/cascades/lbpcascade_frontalface.xml";
         faceDetector = new CascadeClassifier(cascadePath);
+        String cascadeEye = "src/main/resources/cascades/haarcascade_eye_tree_eyeglasses.xml";
+        eyeDetector =new CascadeClassifier(cascadeEye);
     }
 
     private void initGUI() {
@@ -101,20 +104,34 @@ public class App extends Thread {
 
     private void detectAndDrawFace(Mat image) {
         MatOfRect faceDetections = new MatOfRect();
+        MatOfRect eyeDetections = new MatOfRect();
         faceDetector.detectMultiScale(image, faceDetections, 1.1, 7, 0, new Size(250, 40), new Size());
+        eyeDetector.detectMultiScale(image, eyeDetections, 1.3, 2, 0|CASCADE_SCALE_IMAGE, new Size(0, 0), new Size(200, 200) );
         // Draw a bounding box around each face.
+
+
+
         boolean b = false;
-        Rect rectCrop = new Rect(10,10,10,10);
+        Rect rectCrop = new Rect(10, 10, 10, 10);
         for (Rect rect : faceDetections.toArray()) {
             Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
             b = true;
-            rectCrop = new Rect(rect.x,rect.y,rect.width,rect.height);
+            rectCrop = new Rect(rect.x, rect.y, rect.width, rect.height);
         }
-        Mat temp = new Mat(image,rectCrop);
-        Imgproc.resize(temp,temp,new Size(250,250));
-        Imgproc.threshold(temp,temp,25,255,Imgproc.THRESH_BINARY);
+        for(Rect eyesArr : eyeDetections.toArray()){
+
+
+            Imgproc.rectangle(image, new Point(eyesArr.x, eyesArr.y), new Point(eyesArr.x + eyesArr.width, eyesArr.y + eyesArr.height),  new Scalar(255, 0, 0), 4, 8, 0);
+        }
+        Mat temp = new Mat(image, rectCrop);
+        Imgproc.resize(temp, temp, new Size(250, 250));
+
+//        Imgproc.threshold(temp,temp,127,255,Imgcodecs
+//        .CV_LOAD_IMAGE_GRAYSCALE);
+        Imgproc.cvtColor(temp, temp, Imgproc.COLOR_RGB2GRAY);
         if (b)
-            Imgcodecs.imwrite("data.png",temp);
+            Imgcodecs.imwrite("data.png", temp);
+
 
     }
 
